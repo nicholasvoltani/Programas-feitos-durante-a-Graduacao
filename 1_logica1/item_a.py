decb5ca6b599e825559aecf8ee5f6a3cbc00e00d
Nicholas Funari Voltani <nicholasvoltani@gmail.com>
2020-03-22 22:23:45 -0300

    Delete item_a.py

--- 1_logica1/item_a.py
+++
@@ -1,9 +0,0 @@
-def VerificaMinas(listaCPF):
-    '''
-    Dada lista de CPFs, retorna lista somente com CPFs cujo 9º número seja igual a 6.'''
-    for CPF in listaCPF:
-        ##só pra garantir que é string
-        CPFstr=str(CPF)
-        if CPFstr[8]!='6':
-            listaCPF.remove(CPF)
-    return listaCPF
