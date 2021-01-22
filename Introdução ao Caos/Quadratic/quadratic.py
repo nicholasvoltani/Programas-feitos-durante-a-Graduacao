9e5c1ebacc61ec75928ed58ba7c787cea873a47f
nicholasvoltani <nicholasvoltani@gmail.com>
2021-01-22 18:53:03 -0300

    .

--- Introdução ao Caos/Quadratic/quadratic.py
+++ Introdução ao Caos/Quadratic/quadratic.py
@@ -12,10 +12,21 @@ xs = []
 cs = []
 
 
-x1 = (-1 + np.sqrt(1 + 4*C))/2
-x2 = (-1 -  np.sqrt(1 + 4*C))/2
+for c in C:
+    x = 1
+    for _ in range(Ttrans):
+        x = quadratic(x,c)
+    for _ in range(Test):
+        x = quadratic(x,c)
+        xs.append(x)
+        cs.append(c)
 
-plt.plot(C,x1, label = r'x^*_1 =\frac{-1 + \sqrt(1 + 4c}}{2}') 
-plt.plot(C,x2, label = r'x^*_2 =\frac{-1 - \sqrt(1 + 4c}}{2}')
-plt.legend()
+#x1 = (-1 + np.sqrt(1 + 4*C))/2
+#x2 = (-1 -  np.sqrt(1 + 4*C))/2
+
+plt.plot(cs,xs,',')
+#plt.plot(C,x1, label = r'x^*_1 =\frac{-1 + \sqrt(1 + 4c}}{2}') 
+#plt.plot(C,x2, label = r'x^*_2 =\frac{-1 - \sqrt(1 + 4c}}{2}')
+#plt.legend()
+plt.title(r"Bifurcation diagram for $x_{n+1} = C - x_n^2$")
 plt.show()
